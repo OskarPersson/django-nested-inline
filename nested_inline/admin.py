c3da9d51f869785e2b6993c161955e53f95c70a8
matt <matt@asl19.org>
2023-02-23 10:04:56 -0500

    Fixed linters

--- nested_inline/admin.py
+++ nested_inline/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.admin import helpers
 from django.contrib.admin.options import InlineModelAdmin, reverse
 from django.contrib.admin.utils import unquote
-from django.core.exceptions import PermissionDenied, FieldDoesNotExist
+from django.core.exceptions import FieldDoesNotExist, PermissionDenied
 from django.db import models, transaction
 from django.forms.formsets import all_valid
 from django.http import Http404
